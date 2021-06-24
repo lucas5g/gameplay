49b702e605d1e4d147bfa1496f67a63f6c238a6a
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-06-24 00:58:05 -0300

    03 - 1:08

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import { Feather } from "@expo/vector-icons";
 import { View, Text } from 'react-native'
 import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   title: string;
@@ -15,6 +16,13 @@ type Props = {
 export function Header({ title, action }: Props) {
 
   const { secondary100, secondary40, heading } = theme.colors
+
+  const navigation = useNavigation()
+
+  function handleGoBack(){
+    navigation.goBack()
+  }
+
   return (
     <LinearGradient
       style={styles.container}
@@ -26,6 +34,7 @@ export function Header({ title, action }: Props) {
           name='arrow-left'
           size={24}
           color={heading}
+          onPress={() => handleGoBack()}
         />
       </BorderlessButton>
 
