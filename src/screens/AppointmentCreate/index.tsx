00b216f5a8f753556a42ec56592e9668dc47f367
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-06-24 10:49:17 -0300

    aula 3 - 1:31

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -10,7 +10,11 @@ import { styles } from "./styles";
 
 import { CategorySelect } from "../../components/CategorySelect";
 import { Background } from "../../components/Background";
+import { GuildIcon } from "../../components/GuildIcon";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
 import { Header } from "../../components/Header";
+
 
 export function AppointmentCreate() {
 
@@ -22,7 +26,10 @@ export function AppointmentCreate() {
         title='Agendar partida'
 
       />
-      <Text style={styles.label}>
+      <Text style={[
+        styles.label,
+        { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}
+      >
         Categoria
       </Text>
       <CategorySelect
@@ -34,8 +41,12 @@ export function AppointmentCreate() {
       <View style={styles.form}>
         <RectButton>
           <View style={styles.select}>
+            {
+              // <View style={styles.image} />
+              <GuildIcon />
 
-            <View style={styles.image} />
+            }
+
             <View style={styles.selectBody}>
               <Text style={styles.label}>
                 Selecione um servidor
@@ -49,6 +60,53 @@ export function AppointmentCreate() {
 
           </View>
         </RectButton>
+
+        <View style={styles.field}>
+          <View>
+            <Text style={styles.label}>
+              Dia e mês
+            </Text>
+
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>
+                /
+              </Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+          <View>
+            <Text style={styles.label}>
+              Hora e minuto
+            </Text>
+
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>
+                :
+              </Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+
+        </View>
+
+        <View style={[styles.field, {marginBottom: 12}]}>
+          <Text style={styles.label}>
+            Descrição
+          </Text>
+          <Text style={styles.caracteresLimit}>
+            Max 100 caracteres
+          </Text>
+        </View>
+        <TextArea
+          multiline
+          maxLength={100}
+          numberOfLines={5}
+          autoCorrect={false}
+        />
       </View>
     </Background>
   )
--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -24,12 +24,43 @@ export const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 8,
     alignItems: 'center',
-    paddingRight: 25
+    paddingRight: 25,
+    overflow: 'hidden'
   },
 
   selectBody:{
     flex: 1,
     alignItems: 'center',
+  },
+
+  image:{
+    width: 64,
+    height: 68,
+    backgroundColor: theme.colors.secondary50,
+    borderWidth: 1,
+    borderRadius: 8
+  },
+
+  field:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 30
+  },
+  column:{
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  divider:{
+    marginRight: 4,
+    fontSize: 18,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.highlight
+  },
+  caracteresLimit:{
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    color: theme.colors.highlight
   }
 
 })
